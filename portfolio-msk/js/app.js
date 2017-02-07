851823b788de9fa21d468c032443f5dc658c8403
Polina Katysheva <polly995@yandex.ru>
2017-02-07 22:45:52 +0300

    Fixes v.2

--- portfolio-msk/js/app.js
+++ portfolio-msk/js/app.js
@@ -15,22 +15,20 @@
         function(){
           $(div)
           .css('display', 'block')
-          .animate({opacity: 1, top: '50%'}, 200);
+          .animate({opacity: 1}, 200);
         });
       $(body).css('overflow', 'hidden');
       $(modal).css('overflow', 'auto');
     });
     close.click( function(){
       modal
-      .animate({opacity: 0, top: '45%'}, 200,
+      .animate({opacity: 0}, 200,
         function(){
           $(this).css('display', 'none');
           overlay.fadeOut(400);
         });
       $(body).css('overflow', 'auto');
     });
-
-
   })
 
 })(jQuery)
